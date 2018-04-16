eb710373e89a7f0c9cdcdb924989fcfd8ca62fdc
Jeff Jarvis <jjarvis@codehatlabs.com>
2018-04-16 11:08:56 -0700

    add comments

--- pygwanda/helpers.py
+++ pygwanda/helpers.py
@@ -1,9 +1,10 @@
 from random import choice
 
 
+# Omit "lIO10" (lowercase ell, uppercase eye and oh, numbers one and zero)
+#   for reliable human readability.
 UNAMBIGUOUS_ALPHA = 'qwertyuiopasdfghjkzxcvbnm' \
                     'QWERTYUPASDFGHJKLZXCVBNM'
-
 UNAMBIGUOUS_ASCII = UNAMBIGUOUS_ALPHA \
                     + \
                     '23456789' \
