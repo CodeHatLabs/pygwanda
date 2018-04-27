9562c2879770c273713891c0db75220b246a7e5e
Jeff Jarvis <jjarvis@codehatlabs.com>
2018-04-27 11:47:35 -0700

    add convenience method for getting a gmtime string

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,6 +16,21 @@ class TestHelpers:
         for ch in result:
             assert ch in characters
 
+    def test_gmtime_string(self):
+        gmts = gmtime_string()
+        assert len(gmts) == 16
+        assert gmts[8] == 'T'
+        assert gmts[-1] == 'Z'
+        gmts = gmtime_string((2076, 7, 4, 5, 0, 0))
+        assert len(gmts) == 16
+        assert gmts[8] == 'T'
+        assert gmts[-1] == 'Z'
+        assert gmts[:4] == '2076'
+        assert gmts[4:6] == '07'
+        assert gmts[6:8] == '04'
+        assert gmts[9:11] == '05'
+        assert gmts[11:15] == '0000'
+
     def test_random_unambiguous_string(self):
         self._do_characters_test(UNAMBIGUOUS_LOWER)
         self._do_characters_test(UNAMBIGUOUS_UPPER)
